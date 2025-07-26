c489883256d7cb7bfdc26dd59406d8cddde47784
apoda008 <161513572+apoda008@users.noreply.github.com>
2025-07-25 20:04:33 -0700

    MAKING PROGRESS!!

---
+++ Media_Web_Server/Contollers/StreamController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic.ApplicationServices;
+using System.IO;
+
+namespace VideoStreamServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StreamController : ControllerBase
+    {
+        [HttpGet("{fileName}")]
+        public IActionResult GetVideo(string fileName)
+        {
+            //string filePath = Path.Combine("Videos", fileName);
+            string filePath = "C:\\Users\\dan_a\\Desktop\\TestLocationForRepo\\Media_Repository\\Media_Movies\\U571.mp4";
+            if (!System.IO.File.Exists(filePath))
+                return NotFound();
+
+            var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            return File(stream, "video/mp4", enableRangeProcessing: true);
+        }
+    }
+}
--- Media_Web_Server/Program.cs
+++ Media_Web_Server/Program.cs
@@ -14,12 +14,14 @@ using System.Runtime.CompilerServices;
 //////////     SERVER STUFF     ///////////////////////////
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddControllers();
 // Add services to the container.
 builder.Services.AddRazorComponents();
 builder.Services.AddSingleton<MediaRepoConnection>();
 
 var app = builder.Build();
+app.MapControllers();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
